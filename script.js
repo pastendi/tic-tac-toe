d0c6fcec4b30cb198c67e3aaf98ee2dfc4458f9d
Tendi <pastendisherpa26@gmail.com>
2023-03-13 20:26:51 +0545

    check winner and stop bot after win

--- script.js
+++ script.js
@@ -16,45 +16,90 @@ window.onload = () => {
   selectO.onclick = () => {
     selectBox.classList.add('hide')
     playBoard.classList.add('show')
-    players.setAttribute('class', 'players active player')
+    playBoard.setAttribute('class', 'players active player')
   }
 }
 let playerX = 'fas fa-times'
 let playerO = 'far fa-circle'
+let playerSign = ''
+let runBot = true
 
 // human click function
 function clickedBox(element) {
+  playerSign = 'X'
   if (players.classList.contains('player')) {
     element.innerHTML = `<i class="${playerO}"></i>`
     players.classList.add('active')
+    playerSign = 'O'
+    element.setAttribute('id', playerSign)
   } else {
     element.innerHTML = `<i class="${playerX}"></i>`
     players.classList.add('active')
+    element.setAttribute('id', playerSign)
   }
+  selectWinner()
+  playBoard.style.pointerEvents = 'none' //stop user to select more than one during the delay of bot
   element.style.pointerEvents = 'none' //avoid the overwriting of box element
   setTimeout(() => {
     // delay for more realism
-    bot()
+    bot(runBot)
   }, 1000)
 }
 
 // bot click function
-function bot() {
-  let array = []
-  for (let i = 0; i < allBox.length; i++) {
-    if (allBox[i].childElementCount == 0) {
-      array.push(i)
+function bot(runBot) {
+  if (runBot) {
+    playerSign = 'O'
+    let array = []
+    for (let i = 0; i < allBox.length; i++) {
+      if (allBox[i].childElementCount == 0) {
+        array.push(i)
+      }
     }
-  }
-  let randomBox = array[Math.floor(Math.random() * array.length)]
-  if (array.length > 0) {
-    if (players.classList.contains('player')) {
-      allBox[randomBox].innerHTML = `<i class="${playerX}"></i>`
-      players.classList.remove('active')
-    } else {
-      allBox[randomBox].innerHTML = `<i class="${playerO}"></i>`
-      players.classList.remove('active')
+    let randomBox = array[Math.floor(Math.random() * array.length)]
+    if (array.length > 0) {
+      if (players.classList.contains('player')) {
+        allBox[randomBox].innerHTML = `<i class="${playerX}"></i>`
+        players.classList.remove('active')
+        playerSign = 'X'
+        allBox[randomBox].setAttribute('id', playerSign)
+      } else {
+        allBox[randomBox].innerHTML = `<i class="${playerO}"></i>`
+        players.classList.remove('active')
+        allBox[randomBox].setAttribute('id', playerSign)
+      }
+      selectWinner()
     }
+    playBoard.style.pointerEvents = 'auto' //stop user to select more than one during the delay of bot
+    allBox[randomBox].style.pointerEvents = 'none'
   }
-  allBox[randomBox].style.pointerEvents = 'none'
+}
+
+// finding winner
+function getClass(idname) {
+  return document.querySelector('.box' + idname).id
+}
+function checkClass(val1, val2, val3, sign) {
+  if (
+    getClass(val1) == sign &&
+    getClass(val2) == sign &&
+    getClass(val3) == sign
+  ) {
+    return true
+  }
+}
+function selectWinner() {
+  if (
+    checkClass(1, 2, 3, playerSign) ||
+    checkClass(4, 5, 6, playerSign) ||
+    checkClass(7, 8, 9, playerSign) ||
+    checkClass(1, 4, 7, playerSign) ||
+    checkClass(2, 5, 8, playerSign) ||
+    checkClass(3, 6, 9, playerSign) ||
+    checkClass(1, 5, 9, playerSign) ||
+    checkClass(3, 5, 7, playerSign)
+  ) {
+    console.log(playerSign + 'wins')
+    runBot = false
+  }
 }
